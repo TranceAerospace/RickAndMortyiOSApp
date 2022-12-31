472684ff4930e9f353591dfad35cb23ca4348ba3
Trance <xp46290@gmail.com>
2022-12-31 10:15:52 -0500

    Initial Commit

---
+++ Rick And Morty App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick And Morty App
+//
+//  Created by Mark Trance on 12/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
