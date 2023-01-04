018391b47e996ba295afb428e87ccf01b3adc6e8
Trance <xp46290@gmail.com>
2023-01-04 07:36:28 -0500

    Setup basic collection view for characters screen

---
+++ Rick And Morty App/Resources/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  Rick And Morty App
+//
+//  Created by Mark Trance on 1/4/23.
+//
+
+import UIKit
+
+extension UIView {
+  
+  func addSubviews(_ views: UIView...) {
+    views.forEach { addSubview($0) }
+  }
+  
+  
+  
+}
